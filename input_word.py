70f07b7119ac7642255561f0946ab426760a1fb9
Neville Oronni <nevooronni@gmail.com>
2017-10-10 14:51:09 +0300

    added all files

---
+++ input_word.py
@@ -0,0 +1,10 @@
+print("Enter a noun")
+noun = input() 
+
+print("Enter a verb")
+verb = input()
+
+print("Enter an adjective")
+adjective = input()
+
+print(f"You know what, I will tell you a funny story about {noun} it will not be {verb} based but you can you can describe it as {adjective}.")
